6b067d4561d7be4d4161007c23d7d748e0a1ba80
evergreen-david <leesangrok1118@gmail.com>
2020-04-28 17:24:10 +0900

    address comment

--- car/views.py
+++ car/views.py
@@ -3,18 +3,4 @@ import json
 from django.http        import HttpResponse, JsonResponse
 from django.views       import View
 
-from .models            import
-
-class ModelView(View):
-    def post(self,request):
-        data = json.loads(request.body)
-
-
-
-class SeatView(View):
-    def post(self,request):
-        data = json.loads(request.body)
-
-
-
 
