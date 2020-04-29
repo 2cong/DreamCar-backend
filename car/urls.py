ea082614e588a4aa459362168acf0887d95a66dd
shooming <liomily@gmail.com>
2020-04-29 19:10:11 +0900

    수정사항 수정

--- car/urls.py
+++ car/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         path('/summary', SummaryView.as_view()),
         path('/default/<int:mvl_id>', DefaultView.as_view()),
         path('/exterior/<int:mvl_id>', ExteriorView.as_view()),
-	path('/wheel/<int:mvl_id>', WheelView.as_view()),
+        path('/wheel/<int:mvl_id>', WheelView.as_view()),
         path('/caliper/<int:mvl_id>', CaliperView.as_view()),
         path('/accessory', AccessoryView.as_view())
        ]
