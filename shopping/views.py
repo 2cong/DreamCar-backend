c24c7689cf0352612e9adb40be69051fa5d35af9
shooming <liomily@gmail.com>
2020-04-29 19:30:20 +0900

    미작성 shopping views 삭제

--- shopping/views.py
+++ shopping/views.py
@@ -1,20 +1,2 @@
-import json
-
-from django.http        import HttpResponse,JsonResponse
-from django.views       import View
-
-from .models            import *
-from car.models         import *
-
-class UserView(View):
-    def post(self, request):
-        data = Json.load(request.body)
-
-        last_name    = data['last_name']
-        family_name  = data['family_name']
-        birthdat     = data['birthday']
-        phone_number = data['phone_number']
-        address1     = data['address1']
-        address2     = data['address2']
 
         
