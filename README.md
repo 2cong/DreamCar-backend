4d56e75edfb68373604340e78cc5ec26fc0f0fb9
shooming <liomily@gmail.com>
2020-05-01 11:52:47 +0900

    shopping.views 전시장 정보 전달 방식 수정

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# DreamCar-backend
+# Maserati Clone Project
+
+## Introduction
+<a herf='https://www.maserati.com/kr/ko'>Maserati web</a> site clone.
+Develop Member (3 Front-End , 2 Back-End) 
+develop period : '20.04.20. ~ '20.05.01.
+
+## DataBase Modeling
+<a herf="https://aquerytool.com:443/aquerymain/index/?rurl=0d127070-399a-4364-960e-ca9891bd0e8a&">![DB](https://user-images.githubusercontent.com/29778054/80758924-675dfe80-8b71-11ea-87e5-7bbd096a5cf0.PNG)</a>
+Aquery read-only key: 872zs0
+
+## API Documentation
+<a herf="https://documenter.getpostman.com/view/11221306/SzfDy5Ru?version=latest">API document published by postman</a>
+
+## Technologies
+- python
+- Django web framework
+- MySQL
+- AWS EC2, RDS
+- CORS headers
+
+## API
+[GET] Car image code Combine API
+[GET] Exterior information API
+[GET] Wheel information API
+[GET] Caliper information API
+[GET] Seat information API
+[GET] Dashboard information API
+[GET] Steering information API
+[GET] Carpet information API
+[GET] Page information API
+[GET] Summary provide API
+[GET] Car deafault color API
+[POST] CustomCar data save API
+[POST] Test drive application save API
+[POST] CustomCar data save and provide token API
--- shopping/views.py
+++ shopping/views.py
@@ -3,7 +3,7 @@ import json
 from django.http            import HttpResponse,JsonResponse
 from django.views           import View
 from django.core.exceptions import ObjectDoesNotExist
-from django.db              import transaction
+from django.db              import transaction, IntegrityError
 
 from car.models         import ModelVersionLine
 from .models            import  ( User, 
@@ -20,44 +20,45 @@ class UserView(View):
     @transaction.atomic
     def post(self, request):
         try:
-            data = json.loads(request.body)
+            with transaction.atomic():
+                data = json.loads(request.body)
 
-            DriveContactChannel.objects.create(
-                email  = data['email_privacy'],
-                fax    = data['fax'],
-                mail   = data['mail'],
-                sms    = data['sms'],
-                call   = data['call'],
-                sns    = data['sns']
-            )
+                DriveContactChannel.objects.create(
+                    email  = data['email_privacy'],
+                    fax    = data['fax'],
+                    mail   = data['mail'],
+                    sms    = data['sms'],
+                    call   = data['call'],
+                    sns    = data['sns']
+                )
 
-            User.objects.create(
-                first_name            = data['first_name'],
-                last_name             = data['last_name'],
-                birthday              = data['birthday'],
-                phone_number          = data['phone_number'],
-                address_city          = AddressCity.objects.get(id = data['city']),
-                address_detail        = data['address'],
-                email                 = data['email'],
-                gender                = Gender.objects.get(id = data['gender']),
-                drive_contact_channel = DriveContactChannel.objects.last(),
-                privacy_check         = data['privacy_check']
-            )
+                User.objects.create(
+                    first_name            = data['first_name'],
+                    last_name             = data['last_name'],
+                    birthday              = data['birthday'],
+                    phone_number          = data['phone_number'],
+                    address_city          = AddressCity.objects.get(id = data['city']),
+                    address_detail        = data['address'],
+                    email                 = data['email'],
+                    gender                = Gender.objects.get(id = data['gender']),
+                    drive_contact_channel = DriveContactChannel.objects.last(),
+                    privacy_check         = data['privacy_check']
+                )
 
-            TestDriveSchedule.objects.create(
-                model_version_line  = ModelVersionLine.objects.get(id = data['mvl']),
-                store_information   = StoreInformation.objects.get(id = data['store']),
-                expect_date         = ExpectDate.objects.get(id = data['expect_date']),
-                contact_us          = data['contact_us']
-            )
+                TestDriveSchedule.objects.create(
+                    model_version_line  = ModelVersionLine.objects.get(id = data['mvl']),
+                    store_information   = StoreInformation.objects.get(id = data['store']),
+                    expect_date         = ExpectDate.objects.get(id = data['expect_date']),
+                    contact_us          = data['contact_us']
+                )
 
-            TestDrive.objects.create(
-                test_drive_schedule   = TestDriveSchedule.objects.last(),
-                user                  = User.objects.last(),
-                current_car_model     = data['current_car'],
-            ) 
+                TestDrive.objects.create(
+                    test_drive_schedule   = TestDriveSchedule.objects.last(),
+                    user                  = User.objects.last(),
+                    current_car_model     = data['current_car'],
+                ) 
 
-            return HttpResponse(status = 200)
+                return HttpResponse(status = 200)
 
         except KeyError:
             return HttpResponse(status = 400)
@@ -82,8 +83,7 @@ class UserView(View):
         exhibition_list =[
                 {
                   'exhibition_id'   : exhibition.id,
-                  'exhibition_name' : exhibition.name,
-                  'exhibition_city' : exhibition.city
+                  'exhibition_name' : exhibition.city + f" ({exhibition.name})",
                 } for exhibition in exhibition_centers
               ]
 
